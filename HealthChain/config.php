6bf8b19c7df4895d4e1a0edf68db151ff7937e3b
Vineet Kumar Agrawal <31213238+vka007@users.noreply.github.com>
2018-12-16 14:12:14 +0530

    Web Components

---
+++ HealthChain/config.php
@@ -0,0 +1,7 @@
+<?php
+	$dbservername = "sql304.ultimatefreehost.in";
+	$dbusername = "ltm_21811039";
+	$dbpassword = "iloveyou21";
+	$dbname = "ltm_21811039_login_system";
+
+	$link = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname);
---
+++ HealthChain/welcome.php
@@ -0,0 +1,28 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// If session variable is not set it will redirect to login page
+if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
+  header("location: login.php");
+  exit;
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <div class="page-header">
+        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION['username']); ?></b>. Welcome to our site.</h1>
+    </div>
+    <p><a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a></p>
+</body>
+</html>
